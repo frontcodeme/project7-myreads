3427518df2e8a95c8fe8928af4b88e42e94ae99f
Moamen <frontcodeme@gmail.com>
2018-08-08 06:06:00 +0200

    Search functionality

--- src/Book.js
+++ src/Book.js
@@ -3,9 +3,12 @@ import React, { Component } from 'react';
 class Book extends Component {
 	render() {
 		return(
+			let displayThumbnails = this.props.book.imageLinks ?
+			this.props.book.imageLinks.thumbnail :
+			'';
 			<div className="book">
 	          <div className="book-top">
-	            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
+	            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${}")` }}></div>
 	            <div className="book-shelf-changer">
 	              <select
 					onChange={(event) => this.props.moveShelf(
--- src/Search.js
+++ src/Search.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
+import Book from './Book';
+import * as BooksAPI from './BooksAPI'
 
 class Search extends Component {
+	state = {
+		query: '',
+		searchedBooks: []
+	}
+
+	updateQuery = (query) => {
+		this.setState({
+			query: query
+		})
+		this.updateSearchedBooks(query);
+	}
+
+	updateSearchedBooks = () => {
+		if (query) {
+			BooksAPI.search(query).then((searchedBooks) => {
+				if (searchedBooks.error) {
+					this.setState({ searchedBooks: [] })
+				} else {
+					this.setState({ searchedBooks: searchedBooks })					
+				}
+			})
+		} else {
+			this.setState({ searchedBooks: [] })
+		}
+
+	}
 	render () {
+
 		return (
 			<div className="search-books">
 	            <div className="search-books-bar">
 	              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
 	              	<div className="search-books-input-wrapper">
-	                	<input type="text" placeholder="Search by title or author"/>
+	                	<input 
+	                		type="text" 
+	                		placeholder="Search by title or author"
+	                		value={this.state.query}
+	                		onChange={(event) => this.update(event.target.value)}
+	                	/>
 	              	</div>
 	            </div>
 	            <div className="search-books-results">
-	              <ol className="books-grid"></ol>
+	              <ol className="books-grid">
+	              	{
+	              		this.setState.searchedBooks.map(searchedBooks => (
+	              			<li key={searchedBooks.id}>
+		              			<Book
+		              				book={searchedBooks}
+	              				/>
+              				</li>
+	              			))
+	              	}
+	              </ol>
 	            </div>
 	      	</div>
 		);
